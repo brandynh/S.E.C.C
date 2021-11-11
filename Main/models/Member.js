2b39a2881a6658fbd59f966d162634714d65ebbf
Brandyn <hufferbrandyn@gmail.com>
2021-11-10 19:03:23 -0600

    create models and seeds

---
+++ Main/models/Member.js
@@ -0,0 +1,66 @@
+const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
+const sequelize = require('../config/connection');
+
+class Member extends Model {
+  checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password);
+  }
+}
+
+Member.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [8],
+      },
+    },
+    projectLead_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'projectLead',
+        key: 'id'
+      }
+    },
+  },
+  {
+    hooks: {
+      beforeCreate: async (newMemberData) => {
+        newMemberData.password = await bcrypt.hash(newMemberData.password, 10);
+        return newMemberData;
+      },
+      beforeUpdate: async (updatedMemberData) => {
+        updatedMemberData.password = await bcrypt.hash(updatedMemberData.password, 10);
+        return updatedMemberData;
+      },
+      
+    },
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'member',
+  }
+);
+
+module.exports = Member;
