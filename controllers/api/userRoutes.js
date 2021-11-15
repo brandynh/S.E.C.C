8167cf0bbb1ed3c4d817566e2ed7e34d969b7861
Neil Juanillo <37945822+ajax12233512@users.noreply.github.com>
2021-11-14 20:39:59 -0600

    progress on login/logout feature. More to add but no visible progress on the front end yet

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -2,7 +2,6 @@ const router = require('express').Router();
 const { Member } = require('../../models');
 
 
-
 //Returns a list of all user. FOr development only
 router.get('/allUsers', async (req, res)=>{
     try{
@@ -15,19 +14,49 @@ router.get('/allUsers', async (req, res)=>{
 });
 
 router.get('/login', (req, res)=>{
+    if(req.session.loggedIn){
+        res.redirect('/');
+    }
     res.render('login');
+    return;
 });
 
 router.post('/', async (req, res) => {
   
 });
 
+//Handle login
 router.post('/login', async (req, res) => {
-  
+  try{
+    const loggedUser = await Member.findOne({
+        where: {
+            username: req.body.username,
+            password: req.body.password
+        }
+    });
+
+    if(!loggedUser){
+        res.status(400).json({message: 'Incorrect user credentials'});
+    }
+
+    req.session.save(() =>{
+        req.session.loggedIn = true;
+        res.status(200).json({user: loggedUser, message: 'user logged in' })
+    });
+  } catch( err ){
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
 router.post('/logout', (req, res) => {
-    
+    if(req.session.loggedIn){
+        req.session.destroy(()=>{
+            res.status(204).end();
+        });
+    } else {
+        res.status(404).end
+    }
 });
 
 module.exports = router;
--- public/js/logout.js
+++ public/js/logout.js
@@ -1,1 +1,31 @@
+const logout = document.getElementById('logout');
+// logout.addEventListener('click', ()=>{
+//     fetch('api/user/', {
+//         method: 'POST',
+//         header: {'Content-Type': 'applications/json'}
+//     });
+//     if(response.ok){
+//         document.location.replace('/');
+//     } else {
+//         alert('failed to log out');
+//     }
+// });
+const logoutFunction = async () => {    
+    try{
+        const response = await fetch('/api/users/logout', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+        });
+        
+        if (response.ok) {
+            document.location.replace('/');
+        } else {
+            alert('Failed to log out.');
+        }
+    } catch (err){
+        console.log(err);
+    }
+};
 
+
+logout.addEventListener('click', logoutFunction);
