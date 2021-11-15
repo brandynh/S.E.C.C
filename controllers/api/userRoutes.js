9b03ae817cef176d6902b417f34a3ece274ac7f2
Neil Juanillo <37945822+ajax12233512@users.noreply.github.com>
2021-11-15 13:25:13 -0600

    login functionality finally working. Rearanged get and post functions in the controller

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,62 +1,84 @@
 const router = require('express').Router();
+const { response } = require('express');
 const { Member } = require('../../models');
 
 
-//Returns a list of all user. FOr development only
-router.get('/allUsers', async (req, res)=>{
-    try{
-        const allMembers = await Member.findAll();
-        res.status(200).json(allMembers); 
-    } catch(err) {
-        console.log(err);
-        res.status(500).json(err);
-    }
-});
 
-router.get('/login', (req, res)=>{
-    if(req.session.loggedIn){
-        res.redirect('/');
-    }
-    res.render('login');
-    return;
-});
+
+
 
 router.post('/', async (req, res) => {
   
 });
 
 //Handle login
-router.post('/login', async (req, res) => {
-  try{
-    const loggedUser = await Member.findOne({
-        where: {
-            username: req.body.username,
-            password: req.body.password
-        }
+router.post('/signup', async (req, res) =>{
+    console.log(req.body);
+    try{
+      const newMember = await Member.create({
+      name: req.body.name,
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password,
+      is_project_lead: false
     });
-
-    if(!loggedUser){
-        res.status(400).json({message: 'Incorrect user credentials'});
+  
+    // save cookie that saves the login or logout feature
+    req.session.save(()=>{
+      req.session.loggedIn = true;
+      res.status(200).redirect('/');
+    });
+  
+  
+    } catch (err){
+      console.log(err);
+      res.status(500).json(err);
     }
+  });
+  
+  router.post('/login', async (req, res) => {
+    try{
+      const loggedUser = await Member.findOne({
+          where: {
+              username: req.body.username,
+              password: req.body.password
+          }
+      });
+  
+      if(!loggedUser){
+          res.status(400).json({message: 'Incorrect user credentials'});
+      }
+  
+      req.session.save(() =>{
+          req.session.loggedIn = true;
+          console.log('session Id: ' + req.sessionID)
+          req.session.username = req.body.username;
+          req.session.password = req.body.password;
+          res.status(200).json({message: 'user logged in'});
+          console.log(req.session.loggedIn);
+      });
+    } catch( err ){
+      console.log(err);
+      res.status(500).json(err);
+    }
+  });
 
-    req.session.save(() =>{
-        req.session.loggedIn = true;
-        res.status(200).json({user: loggedUser, message: 'user logged in' })
-    });
-  } catch( err ){
-    console.log(err);
-    res.status(500).json(err);
-  }
-});
+
 
 router.post('/logout', (req, res) => {
+    console.log(req.sessionID)
     if(req.session.loggedIn){
         req.session.destroy(()=>{
             res.status(204).end();
+            // req.session.loggedIn = false;
+            // console.log(req.session.loggedIn);
         });
     } else {
-        res.status(404).end
+        res.status(404).end();
     }
 });
+
+
+
 
 module.exports = router;
